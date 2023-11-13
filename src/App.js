4ddcfb1819d3ab232b7d6bfb1597e6138d6dcd53
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-13 15:30:21 +0100

    Complete task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Mareike" isCoach />; // shorthand syntax for boolean props (if the value is true, you can omit the value)
+}
+
+function Greeting({ name, isCoach }) {
+  return <h1>Hello, {isCoach ? name : "there"}!</h1>;
 }
